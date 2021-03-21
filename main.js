d29d9f2a2c33e692080024340f8710e812a8ae63
mohamed1maghraby-div <mohamed1maghraby@yahoo.com>
2021-03-21 04:46:41 +0200

    slider update

--- main.js
+++ main.js
@@ -9,7 +9,41 @@ var typewriter = setInterval(function(){
 }, 100);
 
 
-
+(function () {
+    "use strict";
+  
+    var carousels = function () {
+      $(".owl-carousel1").owlCarousel({
+        loop: true,
+        center: true,
+        margin: 0,
+        autoplay:true,
+        autoplayTimeout:1000,
+        autoplayHoverPause:true,
+        responsiveClass: true,
+        nav: false,
+        responsive: {
+          0: {
+            items: 1,
+            nav: false
+          },
+          680: {
+            items: 2,
+            nav: false,
+            loop: false
+          },
+          1000: {
+            items: 3,
+            nav: true
+          }
+        }
+      });
+    };
+  
+    (function ($) {
+      carousels();
+    })(jQuery);
+  })();
 
 
 
